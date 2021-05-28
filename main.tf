c07a0703ea2a6a34e508c7a7bef67e862df245a0
Daniel Frycowski <frycowskidan@gmail.com>
2021-05-28 14:29:33 +0200

    Added CodeCommit URL output

--- main.tf
+++ main.tf
@@ -94,18 +94,3 @@ module "webservers_module" {
   security_groups  = [module.sg_module.object_sg_HTTP_SSH.name]
   role             = module.ws_role_module.object.name
 }
-
-# For future codeBuild
-#output "jenkins_ip" {
-#    value = module.jenkins.object.public_ip
-#}
-
-# Display IPs of created EC2 Apache web servers
-output "webServers_ips" {
-  value = module.webservers_module.object[*].public_ip
-}
-
-# Display DNS name of created Elastic Load Balancer
-output "loadBalancer_dns_name" {
-  value = module.elb_module.object.dns_name
-}
